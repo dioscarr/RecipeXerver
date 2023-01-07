5bee80f0e73f70f6d3f98d2806d4e5d59263803f
Dioscar <dioscarr@gmail.com>
2023-01-07 11:59:39 -0500

    GetRecipeSuggestions

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/GetRecipeSuggestions', async (req, res) => {
 
   const input = req.body.input;
   console.log(`input: ${input}`)
-  const responseText = await openai.getOpenAIresponse(input);
+  const responseText = await openai.GetRecipeSuggestions(input);
   res.send(responseText);
   
 });
