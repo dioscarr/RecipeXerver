95389ccbfe879d8cba06c85d65f63c218e3bc1ce
Dioscar <dioscarr@gmail.com>
2023-01-07 18:10:00 -0500

    ADDED encoded and decoded

--- openai.js
+++ openai.js
@@ -1,10 +1,10 @@
 const axios =require('axios')
-
+const {encode, decode} = require('gpt-3-encoder')
 async function getOpenAIresponse(input) {
-    console.log("Hello")
+    console.log("getOpenAIresponse")
     try {
       const response = await axios.post('https://api.openai.com/v1/completions', {
-        prompt: `${input}\n`,
+        prompt: `${encoded(input)}\n`,
         model: "text-davinci-003",
         temperature: 0,
         max_tokens: 100,
@@ -23,17 +23,17 @@ async function getOpenAIresponse(input) {
         },
       });
       console.log(`response: ${response.data.choices[0].text}`);
-      return response.data.choices[0].text;
+      return decode(response.data.choices[0].text);
     } catch (error) {
       console.error(error);
     }
   }
 
   async function GetRecipeSuggestions(input) {
-    console.log("Hello")
+    console.log("GetRecipeSuggestions")
     try {
       const response = await axios.post('https://api.openai.com/v1/completions', {
-        prompt: `${input}`,
+        prompt: `${encoded(input)}`,
         model: "text-davinci-003",
         temperature: 0,
         max_tokens: 300,
@@ -51,8 +51,8 @@ async function getOpenAIresponse(input) {
           'Access-Control-Allow-Headers': 'Content-Type',
         },
       });
-      console.log(`response: ${response.data.choices[0].text}`);
-      return response.data.choices[0].text;
+      console.log(`response: ${decode(response.data.choices[0].text)}`);
+      return decode(response.data.choices[0].text);
     } catch (error) {
       console.error(error);
     }
