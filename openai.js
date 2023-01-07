d27b0b4438ab509bed91cd78704474df1b33f2a0
Dioscar <dioscarr@gmail.com>
2023-01-07 17:38:28 -0500

    change promt to pass input

--- openai.js
+++ openai.js
@@ -33,7 +33,7 @@ async function getOpenAIresponse(input) {
     console.log("Hello")
     try {
       const response = await axios.post('https://api.openai.com/v1/completions', {
-        prompt: `${input} recipes\n max of 3 words description`,
+        prompt: `${input}`,
         model: "text-davinci-003",
         temperature: 0,
         max_tokens: 300,
